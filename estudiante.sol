5d172aa4c23b9abd399f48afd4bc66ee088569ab
WaterBottle05 <107226873+WaterBottle05@users.noreply.github.com>
2022-06-11 12:14:22 -0300

    la mitad
    por la mitad

--- estudiante.sol
+++ estudiante.sol
@@ -1,17 +1,61 @@
 pragma soliditi ^0.8.10;
-contract 
+contract student {
 private string nombre;
-private string apellido;
-private string curso;
+private string apellido_;
+private string _curso;
 private string docente;
-
-private mapping(string => uint);
+private address _docente;
+private bool _aprobo:
+private mapping (string => uint) notas_materias;
 //elegimos uint debido a su capacidad de almacenar enteros positivos
 
-constructor (string memory name_ , string memory apellido_ , string memory curso_ )
+constructor (string memory name_ , string memory _apellido, string memory _curso )
 {   
     nombre = name_;
-    apellido = apellido_;
-    curso = curso_;
-    
+    apellido_ = _apellido;
+    curso_ = _curso;
+    _docente = msg.sender;
+   
+}
+
+function apellido() public returns (string memory)
+{
+    return(apellido_);
+
+}
+function nombre_completo() public returns (string)
+{
+    return nombre;
+}
+
+function curso() public returns (uint)
+{
+    return _curso;
+}
+function set_nota_materia( uint nota, string memory materia) public
+{
+    require (msg.sender == _docente, "not owner");
+    require (uint nota >=1 && <==100, "invalid mark");
+    notas_materias [materia] = nota;
+   
+}
+
+function nota_materia (string memory _materia) returns (uint)
+{
+    return notas_materias[_materia];
+}
+
+function aprobo (string memory materia_) returns (bool)
+{
+    aprobo_ = false;
+    notas_materias[materia_] = mark_;
+    if (mark_ >= 60)
+    {
+        aprobo_ = true;
+    }
+    return(aprobo_)
+
+}
+
+
 }
