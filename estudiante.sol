f379023eff812e4134f6c537c1f9b6ab92d97fec
WaterBottle05 <107226873+WaterBottle05@users.noreply.github.com>
2022-06-17 18:44:31 -0300

    final?

--- estudiante.sol
+++ estudiante.sol
@@ -1,59 +1,52 @@
-pragma soliditi ^0.8.10;
+pragma solidity ^0.8.10;
 contract student {
-private string nombre;
-private string apellido_;
-private string _curso;
-private string docente;
-private address _docente;
+ string private nnombre;
+ string private apellido_;
+ string private _curso_;
+ address private _docente;
 //para almacenar la address del owner
-private bool _aprobo:
+  bool private _aprobo;
 //bool cuya funcion explico mas adelante
-private uint promedio_;
-//variable que utilizo para almacenaar el promedio
-private mapping (string => uint) notas_materias;
-//elegimos uint debido a su capacidad de almacenar numeros enteros no negativos
-private mapping (string => bool) inserted;
-//esto lo creo para que una vez se añada una materia en notas_materia, se cree registro de esto en este mapping y no se añada devuelta, en el caso de que ya existia, en el array arr_m
-private string memory[] arr_m;
-//cree un array para poder hacer el promedio con los values del mapping, que, de otra forma serian inaccesibles porque el mapping no tiene posision 
 
-constructor (string memory name_ , string memory _apellido, string memory _curso )
+ mapping (string => uint) notas_materias;
+//elegimos uint debido a su capacidad de almacenar numeros enteros no negativos
+uint [] arr_n;
+//cree un array para poder hacer el promedio con los values del mapping, que, de otra forma serian inaccesibles porque el mapping no tiene posision 
+ 
+
+
+constructor(string memory name_ , string memory _apellido, string memory _curso )
 {   
-    nombre = name_;
+    nnombre = name_;
     apellido_ = _apellido;
-    curso_ = _curso;
+    _curso_ = _curso;
     _docente = msg.sender;   //justo aca declaro que el smg.sender es el address del docente para que luego tenga acceso a la funcion set_nota_materia
 
 }
 
 function apellido() public returns (string memory)
 {
-    return(apellido_);
+    return apellido_;
 
 }
-function nombre_completo() public returns (string)
+function nombre_completo() public returns (string memory,string memory)
 {
-    return nombre;
+    return (nnombre, apellido_);
+     
+   
 }
 
-function curso() public returns (uint)
+function curso() public returns (string memory)
 {
-    return _curso;
+    return _curso_;
 }
 function set_nota_materia( uint nota, string memory materia) public
 {
     //hago dos require para que el numero de la nota sea valida y para que solo el owner pueda llamar a la funcion
     require (msg.sender == _docente, "not owner");
-    require (uint nota >=1 && <==100, "invalid mark");
+    require ( nota >=1 && nota <=100, "invalid mark");
     notas_materias [materia] = nota;
-
-
-    if(!inserted[nota])
-    {
-        inserted[nota] = true;
-        arr_m.push(materia);
-    }
-
+    arr_n.push(nota);
    
 }
 
@@ -65,25 +58,28 @@ function nota_materia (string memory _materia) public returns (uint)
 function aprobo (string memory materia_) public returns (bool)
 {
     //primero creo un bool falso, para que despues, si la nota es mayor a 60 se cambie a true y devolver ese valor 
-    aprobo_ = false;
+    bool aprobo_ = false;
+    uint mark_;
     notas_materias[materia_] = mark_;
     if (mark_ >= 60)
     {
         aprobo_ = true;
     }
-    return(aprobo_)
+    return aprobo_;
 
 }
 
 
 function promedio() public returns(uint)
 {
-    for (i=0;i<arr_m.lenght; i++)
+    uint promedio_;
+    for (int i=0; i < arr_m.lenght ; i++)
     {
-        promedio_ += notas_materias[arr_m[i]];
+        promedio_ += notas_materias[arr_n[i]];
     }
-    promedio_ = promedio_ / arr_m.lenght;
-    return (promedio_);//no hace falta hacer un redondeo manual porque ya esta automatizado el redondeo al cero, es decir la division de dos enteros dara otro entero
+     promedio_ = promedio_ / arr_n.lenght;
+    return promedio_;
+    //no hace falta hacer un redondeo manual porque ya esta automatizado el redondeo al cero, es decir la division de dos enteros dara otro entero
 }
 
 }
